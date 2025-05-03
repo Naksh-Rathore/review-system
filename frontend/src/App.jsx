d1eb02402981f57e1e1926947a8b12debbb0a17f
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-05-02 23:00:09 -0500

    Add simple logic to the create review button and moved the reviews to the center

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import CompanyInfo from "./components/Company-Info/Company-Info.component.jsx"
 import { getAverageReview, ratingToImage } from "./utils/review-methods.util.js"
-import { getReviews } from "./utils/fetch-api.util.js"
+import { getReviews, postReview } from "./utils/fetch-api.util.js"
 import { useEffect, useState } from "react"
 import Review from "./components/Reviews/Reviews.component.jsx"
 import Modal from "react-modal"
@@ -8,21 +8,40 @@ import Modal from "react-modal"
 Modal.setAppElement('#root')
 
 function App() {
-    const [avgImage, setAvgImage] = useState("")
-    const [modalIsOpen, setModalIsOpen] = useState(false)
-    const [reviews, setReviews] = useState([])
+  const [avgImage, setAvgImage] = useState("")
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [reviews, setReviews] = useState([])
+  
+  const [username, setUsername] = useState("")
+  const [rating, setRating] = useState(1)
+  const [comment, setComment] = useState("")
     
-    useEffect(() => {
-        const fetchAndLog = async () => {
-          const res = await getReviews("http://localhost:8080/api/reviews")
-          const numericalReview = getAverageReview(res)
-          setAvgImage(ratingToImage(numericalReview))
+  useEffect(() => {
+    const fetchAndLog = async () => {
+    const res = await getReviews("http://localhost:8080/api/reviews")
+    const numericalReview = getAverageReview(res)
 
-          setReviews(res)
-      }
+    setAvgImage(ratingToImage(numericalReview))
+      setReviews(res)
+    }
     
-        fetchAndLog()
-      }, [])
+      fetchAndLog()
+  }, [reviews.length])
+
+  const createReview = async () => {
+    setModalIsOpen(false)
+
+    if (username.length > 0 && (rating >= 1 && rating <= 5) && comment.length > 0) {
+      const newReview = {
+            username: username, 
+            comment: comment,
+            rating: Number(rating)
+        }
+
+      setReviews(r => [newReview, ...r])
+      await postReview("http://localhost:8080/api/reviews", newReview)
+    }
+  }  
 
   return (
     <div className="container">
@@ -46,11 +65,11 @@ function App() {
           }
         }}
       >
-        <input type="text" placeholder="Enter Username"/>
-        <input type="number" placeholder="Enter Rating"/>
-        <textarea placeholder="Enter Comment"/><br /><br />
+        <input onChange={event => setUsername(event.target.value.trim())} type="text" placeholder="Enter Username"/>
+        <input onChange={event => setRating(event.target.value)} type="number" placeholder="Enter Rating"/>
+        <textarea onChange={event => setComment(event.target.value)} placeholder="Enter Comment"/><br /><br />
         
-        <button onClick={() => setModalIsOpen(false)}>Submit</button>
+        <button onClick={createReview}>Submit</button>
         <button onClick={() => setModalIsOpen(false)}>Close</button>
       </Modal>
 
--- frontend/src/utils/fetch-api.util.js
+++ frontend/src/utils/fetch-api.util.js
@@ -2,13 +2,27 @@ import axios from "axios"
 
 export async function getReviews(url) {
     try {
-        const response = await axios.get(url)
+        const res = await axios.get(url)
 
-        if (response.status !== 200) {
-            throw new Error("Response was not ok!");
+        if (res.status !== 200) {
+            throw new Error("Response was not ok!")
         }
 
-        return response.data
+        return res.data
+    }
+
+    catch(error) {
+        throw new Error(`Could not fetch data: ${error}`)
+    }
+}
+
+export async function postReview(url, review) {
+    try {
+        const res = await axios.post(url, review)
+
+        if (res.status !== 200) {
+            throw new Error("Response was not ok!")
+        }
     }
 
     catch(error) {
