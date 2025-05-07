6edec5d3167df44a6eeebd81a497e3c0a284417b
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-05-06 20:20:25 -0500

    Change limit to 1000 requests per 15 minutes instead of 100 requests per 15 minutes

--- backend/middleware/rateLimiter.middleware.js
+++ backend/middleware/rateLimiter.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
-    max: 100, 
+    max: 1000, 
     message: 'Too many requests from this IP, please try again after 15 minutes',
     standardHeaders: true,
     legacyHeaders: false,  
