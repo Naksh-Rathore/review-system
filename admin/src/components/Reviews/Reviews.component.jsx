3eef1b0a591fc0f9e0c66015c5ce9ac9244834ae
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-05-10 13:04:21 -0500

    Add HTML and CSS for the delete review button

--- admin/src/components/Reviews/Reviews.component.jsx
+++ admin/src/components/Reviews/Reviews.component.jsx
@@ -4,6 +4,10 @@ import { ratingToImage } from "../../utils/review-methods.util.js"
 function Review({ username, rating, comment }) {
     return (
         <div className="review">
+            <div className="delete-btn-container">
+                <button>🗑️</button>
+            </div>
+
             <h3>{username}</h3>
             <h4>Rating: {ratingToImage(rating)}</h4>
             <p>{comment}</p>
